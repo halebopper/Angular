5d834accb9c6e1d3b86a94a658e4c5abb952af58
Hale Bopp <cometbopp@gmail.com>
2013-11-05 21:06:22 +0000

    complete

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 var app = angular.module('phoneApp', []);
 
 app.controller("AppCtrl", function($scope){
-    $scope.leaveVoicemail = function(number, message){
-        alert("Number: "+number+"said: "+message);
+    this.sayHi = function(){
+        alert("Hi!");
     }
+
+    return $scope.AppCtrl = this;
 })
 
-app.directive("panel", function(){
-    return {
-        restrict: "E",
-        transclude: true,
-        template: '<div class="panel" ng-transclude>This is a panel component</div>'
-    };
-})
+//app.directive("panel", function(){
+//    return {
+//        restrict: "E",
+//        transclude: true,
+//        template: '<div class="panel" ng-transclude>This is a panel component</div>'
+//    };
+//})
