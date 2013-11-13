9a4a89d7f56c0aa1320045ba2a6466129a3e96bb
Hale Bopp <cometbopp@gmail.com>
2013-11-13 19:27:44 +0000

    complete

--- src/main.js
+++ src/main.js
@@ -11,6 +11,18 @@ app.config(function($routeProvider){
         })
 })
 
+app.directive("error", function($rootScope){
+    return {
+        restrict: "E",
+        template: "<div class='alert-box alert' ng-show='isError'>Error!!!</div>",
+        link: function(scope) {
+            $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
+                scope.isError = true;
+            })
+        }
+    }
+})
+
 app.controller("AppCtrl", function($rootScope){
     $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
         console.log(rejection);
@@ -29,6 +41,7 @@ app.controller("ViewCtrl", function($scope, $route){
 app.loadData = function($q, $timeout){
     var defer = $q.defer();
     $timeout(function(){
+        //defer.resolve();
         defer.reject("your network is down");
     }, 500);
     return defer.promise;
