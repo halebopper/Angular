a67809edd9d40116c49fd2e8f7764ae93f59f890
Hale Bopp <cometbopp@gmail.com>
2013-11-13 20:31:12 +0000

    complete

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ app.config(function($routeProvider){
     $routeProvider
         .when('/', {
             templateUrl: "app.html",
-            controller: "ViewCtrl",
+            controller: "ViewCtrl"
+        })
+        .when('/new', {
+            templateUrl: "new.html",
+            controller: "NewCtrl",
             resolve: {
-                loadData: app.loadData
+                loadData: viewCtrl.loadData
             }
         })
 })
@@ -23,26 +27,45 @@ app.directive("error", function($rootScope){
     }
 })
 
-app.controller("AppCtrl", function($rootScope){
+app.controller("AppCtrl", function($scope, $rootScope, $route, $location){
     $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
         console.log(rejection);
     })
+    $rootScope.$on("$routeChangeStart", function(event, current, previous, rejection){
+        console.log("start");
+        console.log($scope, $rootScope, $route, $location);
+    })
+    $rootScope.$on("$routeChangeSuccess", function(event, current, previous, rejection){
+        console.log("success");
+        console.log($scope, $rootScope, $route, $location);
+    })
 })
 
-app.controller("ViewCtrl", function($scope, $route){
+app.controller("NewCtrl", function($scope, loadData, $template){
+    console.log("done");
+    console.log($scope, loadData, $template)
+})
+
+var viewCtrl = app.controller("ViewCtrl", function($scope, $route, $location){
 
     console.log($route);
 
+    $scope.changeRoute = function(){
+        console.log("changeRoute invoked");
+        console.log($scope);
+        $location.path("/new");
+    }
+
     $scope.model = {
-        message: "This is my app!!!"
+        message: "Change Route!!!"
     }
 })
 
-app.loadData = function($q, $timeout){
+viewCtrl.loadData = function($q, $timeout){
     var defer = $q.defer();
     $timeout(function(){
-        //defer.resolve();
-        defer.reject("your network is down");
+        defer.resolve({message: "success"});
+        //defer.reject("your network is down");
     }, 500);
     return defer.promise;
 }
