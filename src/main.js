ba1d9b5237a7593ae0d962b242ebded931569409
Hale Bopp <cometbopp@gmail.com>
2013-11-13 20:46:52 +0000

    complete

--- src/main.js
+++ src/main.js
@@ -1,71 +1,30 @@
 var app = angular.module('app', []);
 
-app.config(function($routeProvider){
-    $routeProvider
-        .when('/', {
-            templateUrl: "app.html",
-            controller: "ViewCtrl"
-        })
-        .when('/new', {
-            templateUrl: "new.html",
-            controller: "NewCtrl",
-            resolve: {
-                loadData: viewCtrl.loadData
+app.provider("game", function(){
+    var type;
+    return{
+        setType: function(value){
+            type = value;
+        },
+        $get: function(){
+            return {
+                title: type + "Craft"
             }
-        })
-})
-
-app.directive("error", function($rootScope){
-    return {
-        restrict: "E",
-        template: "<div class='alert-box alert' ng-show='isError'>Error!!!</div>",
-        link: function(scope) {
-            $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
-                scope.isError = true;
-            })
         }
     }
 })
 
-app.controller("AppCtrl", function($scope, $rootScope, $route, $location){
-    $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
-        console.log(rejection);
-    })
-    $rootScope.$on("$routeChangeStart", function(event, current, previous, rejection){
-        console.log("start");
-        console.log($scope, $rootScope, $route, $location);
-    })
-    $rootScope.$on("$routeChangeSuccess", function(event, current, previous, rejection){
-        console.log("success");
-        console.log($scope, $rootScope, $route, $location);
-    })
+
+app.config(function(gameProvider){
+    gameProvider.setType("War");
+});
+
+//app.factory("game", function(){
+//    return {
+//        title: "StarCraft"
+//    }
+//})
+
+app.controller("AppCtrl", function($scope, game){
+    $scope.title = game.title;
 })
-
-app.controller("NewCtrl", function($scope, loadData, $template){
-    console.log("done");
-    console.log($scope, loadData, $template)
-})
-
-var viewCtrl = app.controller("ViewCtrl", function($scope, $route, $location){
-
-    console.log($route);
-
-    $scope.changeRoute = function(){
-        console.log("changeRoute invoked");
-        console.log($scope);
-        $location.path("/new");
-    }
-
-    $scope.model = {
-        message: "Change Route!!!"
-    }
-})
-
-viewCtrl.loadData = function($q, $timeout){
-    var defer = $q.defer();
-    $timeout(function(){
-        defer.resolve({message: "success"});
-        //defer.reject("your network is down");
-    }, 500);
-    return defer.promise;
-}
