47b9cb06ba211a19c54add907c602ece609f2e9e
Hale Bopp <cometbopp@gmail.com>
2013-11-13 20:52:37 +0000

    complete

--- src/main.js
+++ src/main.js
@@ -1,30 +1,34 @@
 var app = angular.module('app', []);
 
-app.provider("game", function(){
-    var type;
-    return{
-        setType: function(value){
-            type = value;
-        },
-        $get: function(){
-            return {
-                title: type + "Craft"
-            }
-        }
+//app.provider("game", function(){
+//    var type;
+//    return{
+//        setType: function(value){
+//            type = value;
+//        },
+//        $get: function(){
+//            return {
+//                title: type + "Craft"
+//            }
+//        }
+//    }
+//})
+
+
+//app.config(function(gameProvider){
+//    gameProvider.setType("War");
+//});
+
+app.factory("game", function(){
+    return {
+        title: "StarCraft"
     }
 })
 
 
-app.config(function(gameProvider){
-    gameProvider.setType("War");
-});
-
-//app.factory("game", function(){
-//    return {
-//        title: "StarCraft"
-//    }
-//})
-
-app.controller("AppCtrl", function($scope, game){
+app.controller("AppCtrl", function($scope, game, $injector){
+    $injector.invoke(function(game){
+        alert(game.title);
+    })
     $scope.title = game.title;
 })
