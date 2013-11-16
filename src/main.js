2282c6d8641165d0d485abebf2505c29bcf7b589
Hale Bopp <cometbopp@gmail.com>
2013-11-16 17:39:18 +0000

    complete

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 var app = angular.module('app', []);
 
-app.run(function($rootScope, $log){
-    $rootScope.$log = $log;
+app.controller("RoomCtrl", function(){
+    this.openDoor = function () {
+        alert("creak");
+    }
+
+    this.buttonTitle = "I'm a button";
 })
